8916c454ed978d70d0f5da7ed5faa6e0049faa70
Shivansh Sharma <100215346+7SHIVANSH7@users.noreply.github.com>
2024-03-13 07:13:31 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-public Class Main{
-  public static void main(String[] args){
-    System.out.println("Hello World!");
-  }
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
